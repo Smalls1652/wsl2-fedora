4f36da21dfa050b094185016be7b401e3c9cfb7d
Timothy Small <smalls.online@icloud.com>
2023-04-24 11:35:23 -0400

    Added cracklib-dicts install

--- config-scripts/init.sh
+++ config-scripts/init.sh
@@ -27,7 +27,7 @@ fi
 dnf upgrade --refresh -y
 
 # Install packages that will be useful in WSL.
-dnf install -y sudo passwd dnf vim wget util-linux readline net-tools openssh openssl zip unzip
+dnf install -y sudo passwd dnf vim wget util-linux readline net-tools openssh openssl zip unzip cracklib-dicts
 
 # Add the default bash profile settings.
 echo -e  "- Adding default Bash profile -"
